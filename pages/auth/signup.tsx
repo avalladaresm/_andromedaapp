d6431674a3358fcafcdbb93a969ee52085bb23aa
avalladaresm <avalladaresm@unitec.edu>
2021-02-07 18:10:36 -0600

    removed redirect instructions from login/signup routes, will fix later

--- pages/auth/signup.tsx
+++ pages/auth/signup.tsx
@@ -1,18 +1,6 @@
-import { useRouter } from 'next/router';
-import { useEffect } from 'react'
 import { SignupCard } from '../../components/signupcard'
 
 export default function Login() {
-  const router = useRouter()
-
-  useEffect(() => {
-    const parsedCookie = {
-      u: document.cookie.split(';')[0]?.split('=')[1],
-      a_t: document.cookie.split(';')[1]?.split('=')[1]
-    }
-
-    if (parsedCookie.u && parsedCookie.a_t) router.push('/')
-  }, [])
 
   return (
     <div className='w-full h-screen bg-gradient-to-tl from-coolGray-300 to-blueGray-500 flex justify-center'>
