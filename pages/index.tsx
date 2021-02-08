0a83b413c3220e3211dc0b3a1cf98e1ae8f6cf26
avalladaresm <avalladaresm@unitec.edu>
2021-02-07 18:13:49 -0600

    removed render Login condition on index, Login show be displayed only
    when route is /auth/login or when pushed to that route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Login from '../pages/auth/login';
 import Navigation from '../components/navigation'
 import { CurrentUserAuthData } from '../models/CurrentUserAuthData';
 import { useAuth } from '../services/auth';
@@ -18,8 +17,7 @@ const Home = () => {
       or={
         <Mayre
           of={<div>Signing you in...</div>}
-          or={<Login />}
-          when={!auth?.r}
+          when={!!auth?.r}
         />
       }
       when={!!auth?.u && !!auth?.r}
