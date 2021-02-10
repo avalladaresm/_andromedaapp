52bfeff39fe5fbca9973d29a1910af41c3162ce8
avalladaresm <avalladaresm@unitec.edu>
2021-02-10 16:44:01 -0600

    changed content div margin left when sidebar collapsed

--- components/navigation/Content.tsx
+++ components/navigation/Content.tsx
@@ -2,7 +2,7 @@ import ContentHeader from "./ContentHeader"
 
 const Content = (props) => {
   return (
-    <div className={`p-3 ${props.collapsed ? 'ml-20' : 'ml-56'} bg-blueGray-300 min-h-screen space-y-2`}>
+    <div className={`p-3 ${props.collapsed ? 'ml-14 sm:ml-20' : 'ml-14 sm:ml-56'} bg-blueGray-300 min-h-screen space-y-2`}>
       <ContentHeader header={props.header} />
       <div className='p-6 bg-blueGray-50 rounded-md shadow-2xl min-h-full'>
         {props.children}
