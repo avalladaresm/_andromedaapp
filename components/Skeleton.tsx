31cfeb9be3bcb115f5d6a17d399ffab9a42e76b5
avalladaresm <avalladaresm@unitec.edu>
2021-02-10 06:25:36 -0600

    changed skeleton color to a slightly darker one

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -2,7 +2,7 @@ export const TextSkeleton = () => {
 
   return (
     <div className="animate-pulse w-full">
-        <div className="m-1 h-3 bg-blueGray-200 rounded-sm"></div>
+        <div className="m-1 h-3 bg-blueGray-400 rounded-sm"></div>
     </div>
   )
 }
