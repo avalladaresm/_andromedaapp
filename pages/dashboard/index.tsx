58185d77d5aa5092d2d9ab76ece507007ad96aba
avalladaresm <avalladaresm@unitec.edu>
2021-02-10 16:42:07 -0600

    added responsive (somewhat) styling to dashboard content div

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -18,10 +18,12 @@ export default function Dashboard() {
       of={
         <MainContainer header='Dashboard'>
           {auth?.r.includes('SUPREME_LEADER') &&
-            <div className='flex flex-col p-3 rounded-md bg-blueGray-200 shadow-md w-1/4 items-center'>
-              <p className='text-center font-semibold text-2xl'>Recent user login activity</p>
-              <p className='text-center'>Showing the 10 most recent records, click <a onClick={() => router.push('/loginhistory')}>here</a> to view complete login history</p>
-              <RecentAuthLogRecords />
+            <div className='flex flex-wrap'>
+              <div className='flex flex-col m-3 p-3 rounded-md bg-blueGray-200 shadow-md sm:w-1/2 lg:w-1/3 xl:w-1/4 2xl:w-1/5 items-center'>
+                <p className='text-center font-semibold text-2xl'>Recent user login activity</p>
+                <p className='text-center'>Showing the 10 most recent records, click <a onClick={() => router.push('/loginhistory')}>here</a> to view complete login history</p>
+                <RecentAuthLogRecords />
+              </div>
             </div>
           }
         </ MainContainer>
