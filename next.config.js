0a38c9b48d236095c2b312c4d96eae1e2973f789
avalladaresm <avalladaresm@unitec.edu>
2021-02-12 06:12:54 -0600

    added some more config for deployment, configuring static routes

--- next.config.js
+++ next.config.js
@@ -6,5 +6,11 @@ module.exports = withBundleAnalyzer({
   env: {
     API_BASE_URL: process.env.API_BASE_URL,
     IPDATA_APIKEY: process.env.IPDATA_APIKEY
+  },
+  trailingSlash: true,
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' }
+    };
   }
 })
