2aa8b236da0f0e8d278db022d8602ada81226f67
avalladaresm <avalladaresm@unitec.edu>
2021-02-27 12:25:15 -0600

    fixed styling on mobile for settings drawer

--- components/SettingsDrawer.tsx
+++ components/SettingsDrawer.tsx
@@ -72,12 +72,12 @@ export const SettingsDrawer = (props) => {
   return (
     <div>
       {props.isOpen &&
-        <div className={`flex flex-col fixed top-12 bottom-0 right-0 overflow-y-auto flex-no-wrap overflow-hidden bg-blueGray-300 z-10 pt-4 pb-2 w-20 px-2 sm:w-60 sm:px-4`}>
+        <div className={`flex flex-col fixed top-12 bottom-0 right-0 overflow-y-auto flex-no-wrap overflow-hidden bg-blueGray-300 z-10 pt-4 pb-2 w-52 px-3 sm:w-60 sm:px-4`}>
           <div className='space-y-3'>
             <div>
               Visible columns
             </div>
-            <div className='flex flex-col space-y-1'>
+            <div className='space-y-1'>
               {settings?.map((s, i) => (
                 <div key={i} className='space-x-1'>
                   <input key={'i' + i} type='checkbox' value={s.Header} defaultChecked={s.checked} className='space-y-3' onChange={(e) => updateVisibleColumnIsChecked(e)} />
--- components/navigation/Content.tsx
+++ components/navigation/Content.tsx
@@ -2,8 +2,10 @@ import ContentHeader from "./ContentHeader"
 
 const Content = (props) => {
   return (
-    <div className={`p-2 ${props.collapsed ? 'ml-14 sm:ml-20' : 'ml-14 sm:ml-56'} bg-blueGray-300 min-h-screen space-y-2
-    ${props.isOpen && 'mr-60 sm:mr-60'}`}>
+    <div className={`p-2 bg-blueGray-300 min-h-screen space-y-2
+      ${props.collapsed ? 'ml-14 sm:ml-20' : 'ml-14 sm:ml-56'} 
+      ${props.isOpen && 'sm:mr-60'}`}
+    >
       <ContentHeader header={props.header} />
       <div className='p-6 bg-blueGray-50 rounded-md min-h-full'>
         {props.children}
